0bf57b59d4318c88b03df10f697c067976db5ca9
Khairul122 <khairulhuda242@gmail.com>
2024-08-17 21:12:55 +0700

    add db

--- pos_minimarket.sql
+++ pos_minimarket.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 15, 2024 at 05:23 PM
+-- Generation Time: Aug 17, 2024 at 02:12 PM
 -- Server version: 8.0.30
 -- PHP Version: 7.4.3
 
@@ -62,7 +62,6 @@ CREATE TABLE `tbl_keranjang` (
 CREATE TABLE `tbl_pelanggan` (
   `pelanggan_id` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
   `pelanggan_nama` text COLLATE utf8mb4_general_ci NOT NULL,
-  `pelanggan_alamat` text COLLATE utf8mb4_general_ci NOT NULL,
   `pelanggan_no_hp` varchar(15) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -70,8 +69,9 @@ CREATE TABLE `tbl_pelanggan` (
 -- Dumping data for table `tbl_pelanggan`
 --
 
-INSERT INTO `tbl_pelanggan` (`pelanggan_id`, `pelanggan_nama`, `pelanggan_alamat`, `pelanggan_no_hp`) VALUES
-('PLGGN00001', 'Udin', '12321', '12321');
+INSERT INTO `tbl_pelanggan` (`pelanggan_id`, `pelanggan_nama`, `pelanggan_no_hp`) VALUES
+('PLGGN00001', 'Udin', '12321'),
+('PLGGN00002', 'Budi1', '082165443677');
 
 -- --------------------------------------------------------
 
@@ -114,7 +114,8 @@ CREATE TABLE `tbl_pengguna` (
 
 INSERT INTO `tbl_pengguna` (`pengguna_id`, `pengguna_nama`, `username`, `password`, `pengguna_pilihan`) VALUES
 (1, 'Admin', 'admin', 'admin', 'Admin'),
-(3, 'AJO ZZ', 'A', 'A', 'Karyawan');
+(3, 'AJO ZZ', 'A', 'A', 'Karyawan'),
+(4, 'budi', 'budi', 'budi', 'Karyawan');
 
 -- --------------------------------------------------------
 
@@ -140,7 +141,7 @@ CREATE TABLE `tbl_produk` (
 INSERT INTO `tbl_produk` (`produk_id`, `produk_kategori`, `produk_merek`, `produk_nama`, `produk_stok`, `produk_harga_modal`, `produk_harga_jual`, `produk_satuan`) VALUES
 ('PRDK000001', '3', '1', '1', 0, 1, 1, '1'),
 ('PRDK000002', '3', '2', '2', 1, 2, 2, '2'),
-('PRDK000003', '3', 'Tekiro', 'Tang Jepit ', 12, 15000, 22000, 'Pcs');
+('PRDK000003', '3', 'Tekiro', 'Tang Jepit ', 955, 15000, 22000, 'Pcs');
 
 -- --------------------------------------------------------
 
@@ -157,6 +158,9 @@ CREATE TABLE `tbl_transaksi` (
   `jumlah_item` int NOT NULL,
   `total_harga_m` int NOT NULL,
   `total_harga` int NOT NULL,
+  `total_bayar` int NOT NULL,
+  `kembalian` int NOT NULL,
+  `diskon` int NOT NULL DEFAULT '0',
   `pengguna` text COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -164,12 +168,13 @@ CREATE TABLE `tbl_transaksi` (
 -- Dumping data for table `tbl_transaksi`
 --
 
-INSERT INTO `tbl_transaksi` (`keranjang_id`, `transaksi_id`, `tgl_wktu_transaksi`, `pelanggan_id`, `produk_id`, `jumlah_item`, `total_harga_m`, `total_harga`, `pengguna`) VALUES
-(18, 'TRNSKS000000005', '10/02/2024 15:41:08', 'PLGGN00001', 'PRDK000003', 2, 30000, 44000, 'Admin'),
-(19, 'TRNSKS000000005', '10/02/2024 15:41:08', 'PLGGN00001', 'PRDK000003', 3, 45000, 66000, 'Admin'),
-(23, 'TRNSKS000000006', '2024-08-15 20:43:03', 'PLGGN00001', 'PRDK000003', 1, 15000, 22000, 'Admin'),
-(24, 'TRNSKS000000006', '2024-08-15 20:43:03', 'PLGGN00001', 'PRDK000003', 1, 15000, 22000, 'Admin'),
-(25, 'TRNSKS000000006', '2024-08-15 20:43:03', 'PLGGN00001', 'PRDK000003', 1, 15000, 22000, 'Admin');
+INSERT INTO `tbl_transaksi` (`keranjang_id`, `transaksi_id`, `tgl_wktu_transaksi`, `pelanggan_id`, `produk_id`, `jumlah_item`, `total_harga_m`, `total_harga`, `total_bayar`, `kembalian`, `diskon`, `pengguna`) VALUES
+(38, 'TRNSKS000000001', '2024-08-17 19:30:55', 'PLGGN00001', 'PRDK000003', 2, 30000, 44000, 200000, 61400, 15400, 'Admin'),
+(39, 'TRNSKS000000001', '2024-08-17 19:30:55', 'PLGGN00001', 'PRDK000003', 5, 75000, 110000, 200000, 61400, 15400, 'Admin'),
+(40, 'TRNSKS000000002', '2024-08-17 20:02:20', 'PLGGN00001', 'PRDK000003', 2, 30000, 44000, 150000, 31200, 13200, 'Admin'),
+(41, 'TRNSKS000000002', '2024-08-17 20:02:20', 'PLGGN00001', 'PRDK000003', 2, 30000, 44000, 150000, 31200, 13200, 'Admin'),
+(42, 'TRNSKS000000002', '2024-08-17 20:02:20', 'PLGGN00001', 'PRDK000003', 2, 30000, 44000, 150000, 31200, 13200, 'Admin'),
+(43, 'TRNSKS000000003', '2024-08-17 20:22:23', 'PLGGN00002', 'PRDK000003', 1, 15000, 22000, 50000, 28000, 0, 'Admin');
 
 --
 -- Indexes for dumped tables
@@ -225,13 +230,13 @@ ALTER TABLE `tbl_transaksi`
 -- AUTO_INCREMENT for table `tbl_kategori`
 --
 ALTER TABLE `tbl_kategori`
-  MODIFY `kategori_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `kategori_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_keranjang`
 --
 ALTER TABLE `tbl_keranjang`
-  MODIFY `keranjang_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `keranjang_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `tbl_pengaturan`
@@ -243,7 +248,7 @@ ALTER TABLE `tbl_pengaturan`
 -- AUTO_INCREMENT for table `tbl_pengguna`
 --
 ALTER TABLE `tbl_pengguna`
-  MODIFY `pengguna_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `pengguna_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
